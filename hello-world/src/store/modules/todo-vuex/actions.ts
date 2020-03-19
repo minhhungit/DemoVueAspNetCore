194d6b3f8e3f0d7e2f3db2811a1eb73a27be412c
Jin <it.minhhung@gmail.com>
2020-03-20 04:15:55 +0700

    demo call api

--- hello-world/src/store/modules/todo-vuex/actions.ts
+++ hello-world/src/store/modules/todo-vuex/actions.ts
@@ -1,6 +1,7 @@
 import { ActionTree } from 'vuex';
 import { TodoState, TodoItem } from './state';
 import { RootState } from '@/store/state';
+// import { Modal } from 'ant-design-vue';
 
 export const actionTypesWithModuleName = {
   LOAD_TODO_ITEMS: 'todoModule/loadTodoItems',
@@ -16,9 +17,17 @@ export const actionTypes = {
   UPDATE_TODO: 'update',
 };
 
+interface Employee {
+  id: number;
+  employee_name: string;
+  employee_salary: number;
+  employee_age: number;
+  profile_image: string;
+}
+
 export const actions: ActionTree<TodoState, RootState> = {
   // @TODO: fetch data from api
-  loadTodoItems({ commit }): void {
+  async loadTodoItems({ commit }): Promise<void> {
     const todoItems = [
       { id: 1, name: 'AAAA' },
       { id: 2, name: 'BBB' },
@@ -26,6 +35,26 @@ export const actions: ActionTree<TodoState, RootState> = {
       { id: 4, name: 'DDDD' },
     ];
     commit('setTodoItems', todoItems);
+
+    // const fetchEmployees = async (): Promise<Array<Employee>> => {
+    //   let result: Employee[] = [];
+    //   const api = 'http://dummy.restapiexample.com/api/v1/employees';
+    //   try {
+    //     const response = await fetch(api);
+    //     const { data } = await response.json();
+    //     result = data;
+    //   } catch (error) {
+    //     Modal.error({
+    //       title: 'Error',
+    //       content: 'Something wrong, can not call api',
+    //     });
+    //   }
+
+    //   return result;
+    // };
+
+    // const x = await fetchEmployees();
+    // console.log(x);
   },
   add({ commit }, name: string): void {
     // @TODO: make the api request before commnitting to the store
